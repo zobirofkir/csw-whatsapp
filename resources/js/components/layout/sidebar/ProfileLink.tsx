3e388dee700f1c516337d7c4fa36c6e8f0d9bf6b
zobirofkir <zobirofkir19@gmail.com>
2025-03-07 14:49:50 +0000

    add auth user name

--- resources/js/components/layout/sidebar/ProfileLink.tsx
+++ resources/js/components/layout/sidebar/ProfileLink.tsx
@@ -1,8 +1,12 @@
+import { usePage } from '@inertiajs/react';
+
 export default function ProfileLink() {
+    const { auth } = usePage().props as any;
+
     return (
         <div className="flex items-center space-x-2">
-            <img src="https://i.pravatar.cc/150?img=12" alt="Profile" className="h-8 w-8 rounded-full" />
-            <span className="text-black dark:text-white">John Doe</span>
+            <img src={auth?.user?.avatar} alt="Profile" className="h-8 w-8 rounded-full" />
+            <span className="text-black dark:text-white">{auth?.user?.name}</span>
         </div>
     );
 }
