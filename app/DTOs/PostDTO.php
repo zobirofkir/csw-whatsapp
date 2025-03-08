06dfd85c63a7c8b5c8e17694f27e5387b4a6a338
zobirofkir <zobirofkir19@gmail.com>
2025-03-08 00:22:30 +0000

    make routes documented

--- app/DTOs/PostDTO.php
+++ app/DTOs/PostDTO.php
@@ -2,8 +2,17 @@
 
 namespace App\DTOs;
 
+/**
+ * Data Transfer Object for Post entities
+ */
 class PostDTO
 {
+    /**
+     * @param string $content The main content of the post
+     * @param string|null $feeling The feeling/emotion associated with the post
+     * @param string|null $activity The activity associated with the post
+     * @param array $media Array of media attachments for the post
+     */
     public function __construct(
         public readonly string $content,
         public readonly ?string $feeling = null,
@@ -11,6 +20,12 @@ class PostDTO
         public readonly array $media = []
     ) {}
 
+    /**
+     * Creates a PostDTO instance from an array of data
+     *
+     * @param array $data Array containing post data with keys: content, feeling, activity, media
+     * @return self
+     */
     public static function fromArray(array $data): self
     {
         return new self(
