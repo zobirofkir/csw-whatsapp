34c101f51594660676641b52cfd656784e4ef858
zobirofkir <zobirofkir19@gmail.com>
2025-03-08 00:21:49 +0000

    cmake routes documented

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -5,23 +5,41 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Services\PostService;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
 
+/**
+ * Controller for handling page-related routes
+ */
 class PageController extends Controller
 {
+    /**
+     * Create a new PageController instance
+     */
     public function __construct(
         private PostService $postService
     ) {}
 
+    /**
+     * Display the welcome page
+     */
     public function welcome()
     {
         return Inertia::render('welcome');
     }
 
+    /**
+     * Display the account page for authenticated users
+     */
     public function account()
     {
         return Inertia::render('account');
     }
 
+    /**
+     * Display the account page for a specific username
+     *
+     * @param string $username The username to display account for
+     */
     public function accountAuth(string $username)
     {
         return Inertia::render('account', [
@@ -29,6 +47,11 @@ class PageController extends Controller
         ]);
     }
 
+    /**
+     * Display a single post
+     *
+     * @param Post $post The post to display
+     */
     public function showPost(Post $post)
     {
         $formattedPost = $this->postService->formatPost($post, auth()->user());
